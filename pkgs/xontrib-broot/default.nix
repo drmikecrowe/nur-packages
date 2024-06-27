1698cc0673a3b7875775243561f41a194848ad1a
Mark Bestley <37385+bestlem@users.noreply.github.com>
2024-06-27 17:58:25 +0100

    feat: Add xontrib-broot
    It builds but I can't test as that means getting my fork setup as
    a NUR repo, which seems a bit too much effort.

---
+++ pkgs/xontrib-broot/default.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  python3,
+}: let
+  pname = "xontrib-broot";
+  version = "0.2.1";
+in
+  python3.pkgs.buildPythonPackage {
+    inherit pname version;
+
+    src = pkgs.fetchFromGitHub {
+      owner = "jnoortheen";
+      repo = "xontrib-broot";
+      rev = "6f658ff88aba27b921017297d8c2c3dfb2ffa332";
+      sha256 = "sha256-9GqsTVCMvrWpTopHtEdicTyYRQzP1NVtQHZsfBT+fUg=";
+    };
+
+    doCheck = true;
+    format = "pyproject";
+    # nativeBuildInputs = with pkgs.python3Packages; [
+    #   setuptools
+    #   poetry-core
+    # ];
+    buildInputs = with pkgs.python3Packages; [
+      setuptools
+      pdm-pep517
+      poetry-core
+    ];
+
+    meta = {
+      homepage = "https://github.com/jnoortheen/xontrib-broot";
+      license = pkgs.lib.licenses.mit;
+      description = "[how-to use in nix](https://github.com/drmikecrowe/nur-packages) xonsh direnv";
+    };
+  }
