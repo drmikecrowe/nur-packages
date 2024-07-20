b29315681a7a0aefd9ddcfdb5223f2d9fcbaf90f
Mike Crowe <drmikecrowe@gmail.com>
2024-07-20 17:29:34 -0400

    "testing changes"

--- lib/default.nix
+++ lib/default.nix
@@ -3,15 +3,4 @@ with pkgs.lib; {
   # Add your library functions here
   #
   # hexint = x: hexvals.${toLower x};
-  maintainers =
-    lib.maintainers
-    // {
-      drmikecrowe = {
-        github = "drmikecrowe";
-        githubId = 90312;
-        name = "Mike Crowe";
-        email = "drmikecrowe@gmail.com";
-        keys = [{fingerprint = "8978 4645 9539 BD9C 18F5  0B5F 6741 8918 CA9B B7B1";}];
-      };
-    };
 }
